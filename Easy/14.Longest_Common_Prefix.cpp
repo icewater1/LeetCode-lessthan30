39a1917c2beb620fe4bd3c275d660b738e7d17f9
icewater1 <64729262+icewater1@users.noreply.github.com>
2020-05-31 02:08:34 +0800

    Update 14.Longest_Common_Prefix.cpp

--- Easy/14.Longest_Common_Prefix.cpp
+++ Easy/14.Longest_Common_Prefix.cpp
@@ -3,45 +3,26 @@ public:
     string longestCommonPrefix(vector<string>& strs)
     {
         bool match = true;
-        int commonEnd = 0, minLength = 0;
+        int commonEnd = -1, minLength = 0;
         string result = "";
         
-        if(strs.size() == 0)
-        {
-            return result;
-        }
-        else if(strs.size() == 1)
-        {
-            return strs[0];
-        }
-        else
-        {
-            minLength = strs[0].length();
-        }
+        if(strs.size() == 0) return result;
+        else if(strs.size() == 1) return strs[0];
         
+        minLength = strs[0].length();
         for(int i = 1; i<strs.size();i++)
         {
-            int curLen = strs[i].length();
-            minLength = (curLen < minLength)? curLen : minLength;
+            minLength = (strs[i].length() < minLength)? strs[i].length() : minLength;
         }
         for(int k = 0; match && k<minLength;k++)
         {
-            int j = 1;
-            while(match && j<strs.size())
+            for(int j = 1; match && j<strs.size(); j++)
             {
                 match = strs[j-1][k] == strs[j][k];
-                if(match && j == strs.size() - 1)
-                {
-                    commonEnd = k;
-                    result = strs[j].substr(0,commonEnd+1);
-                }
-                if(!match)
-                {
-                    break;
-                }
-                j++;
+                if(match && j == strs.size() - 1) commonEnd = k;
+                if(!match) break;
             }
         }
-        return result;
+        return strs[0].substr(0,commonEnd+1);
     }
 };
