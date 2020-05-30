53cfef0aee59b19312db9a15fe5ac256db419251
icewater1 <64729262+icewater1@users.noreply.github.com>
2020-05-31 02:45:05 +0800

    Update 21.Merge_Two_Sorted_Lists.cpp

--- Easy/21.Merge_Two_Sorted_Lists.cpp
+++ Easy/21.Merge_Two_Sorted_Lists.cpp
@@ -9,63 +9,32 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        bool firNull = l1 == nullptr;
-        bool secNull = l2 == nullptr;
-        if((firNull && secNull) || (!firNull && secNull))
-        {
-            return l1;
-        }
-        else if(!secNull && firNull)
-        {
-            return l2;
-
-        }
+        if((!l1 && !l2) || (l1 && !l2)) return l1;
+        else if(l2 && !l1) return l2;
         else
         {
-            ListNode* p1 = l1;
-            ListNode* p2 = l2;
-            int head = 0;
-            if(p1->val <= p2->val)
-            {
-                head = p1->val;
-                p1 = p1->next;
-            }
-            else
-            {
-                head = p2->val;
-                p2 = p2->next;
-            }
-            ListNode* res = new ListNode(head);
-            ListNode* p = res;
-            while(p1 != nullptr && p2 != nullptr)
+            ListNode* p1 = l1, *p2 = l2;
+            int head = min(p1->val,p2->val);
+            if(p1->val <= p2->val) p1 = p1->next;
+            else p2 = p2->next;
+            
+            ListNode* res = new ListNode(head), *p = res;
+            while(p1 && p2)
             {
                 int a = p1->val, b = p2->val;
-                if( a <= b )
+                if( a < b )
                 {
                     p1 = p1->next;
                     p->next = new ListNode(a);
-                    if(a == b)
-                    {
-                        p2 = p2->next;
-                        p = p->next;
-                        p->next = new ListNode(b);
-                    }
                 }
-                else
+                else if(a>=b)
                 {
                     p2 = p2->next;
                     p->next = new ListNode(b);
                 }
                 p = p->next;
             }
-            if(p1 != nullptr && p2 == nullptr)
-            {
-                p->next = p1;
-            }
-            if(p1 == nullptr && p2 != nullptr)
-            {
-                p->next = p2;
-            }
+            p->next = (p1 && !p2)? p1:(!p1 && p2)?p2:nullptr;
             return res;
         }
     }
